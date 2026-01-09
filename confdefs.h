a44a86761ec65177b101f7092c2115e32b349cd3
EC2 Default User <ec2-user@ip-172-31-10-83.us-east-2.compute.internal>
2026-01-09 05:00:14 +0000

    a

--- confdefs.h
+++
@@ -1,7 +0,0 @@
-/* confdefs.h */
-#define PACKAGE_NAME "crun"
-#define PACKAGE_TARNAME "crun"
-#define PACKAGE_VERSION "UNKNOWN"
-#define PACKAGE_STRING "crun UNKNOWN"
-#define PACKAGE_BUGREPORT "giuseppe@scrivano.org"
-#define PACKAGE_URL ""
